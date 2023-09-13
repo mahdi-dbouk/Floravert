5fe4ac92bfff636447ae3bc1e82bd01c938fba29
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-13 21:35:08 +0300

    [Backend] Created User Model

---
+++ backend/dist/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { scannedPlantSchema } from "./scannedplants.model.js";
+import postSchema from "./schemas/post.schema.js";
+import messageSchema from "./schemas/message.schema.js";
+import contactSchema from "./schemas/contact.schema.js";
+const { model, Schema } = mongoose;
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    age: { type: Number, required: true },
+    phone: { type: String, required: true },
+    picture_url: { type: String, required: true },
+    isTrader: { type: Boolean, required: false },
+    posts: [postSchema],
+    scannedPlants: [scannedPlantSchema],
+    messages: [messageSchema],
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    followed: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    contacts: [contactSchema]
+});
+const User = model('User', userSchema);
+export default User;
+//# sourceMappingURL=user.model.js.map
