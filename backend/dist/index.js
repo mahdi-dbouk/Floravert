63068e60e9ab63c1b404abdce2bcf2906c8b3e02
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-13 21:30:18 +0300

    [Backend] Declared Necessary Dependencies

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import 'dotenv/config';
+import mongoDBConn from "./config/connection.js";
+import cors from 'cors';
+import options from "./config/cors.js";
 const PORT = process.env.PORT || 8000;
 const app = express();
-app.get('/', (req, res) => {
-    return res.json({
-        message: "testing",
-    });
-});
+app.use(cors(options));
+app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
+mongoDBConn();
 //# sourceMappingURL=index.js.map
