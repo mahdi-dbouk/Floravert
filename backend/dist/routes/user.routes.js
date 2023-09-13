7b019b0550a5ae91946ba21857a005aa59d15e17
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-14 00:43:34 +0300

    [Backend] Fixed a Typo in Login Route

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { login } from '../controllers/auth.controller.js';
-import { isAuthorized } from '../middlewares/auth.middleware.js';
 let router = Router();
-router.post("/login", isAuthorized, login);
+router.post("/login", login);
 export default router;
 //# sourceMappingURL=user.routes.js.map
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,6 +3,6 @@ import {login} from '../controllers/auth.controller.js';
 import {isAuthorized} from '../middlewares/auth.middleware.js';
 let router : Router = Router();
 
-router.post("/login",isAuthorized, login);
+router.post("/login", login);
 
 export default router;
