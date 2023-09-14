9b5d2ce6bdbda1c8fa1969a1d471d2e27b03e5e8
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-14 13:40:59 +0300

    [Backend] Implemented Register Route with Proper Validation

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import { login } from '../controllers/auth.controller.js';
+import { login, register } from '../controllers/auth.controller.js';
+import { check } from "express-validator";
 let router = Router();
 router.post("/login", login);
+router.post("/register", [
+    check('firstName').notEmpty().withMessage("first name must not be empty"),
+    check('lastName').notEmpty().withMessage("first name must not be empty"),
+    check('email').isEmail().withMessage("Incorrectly formatted email"),
+    check('password').isLength({ min: 6 }).withMessage("password should be at least 6 characters long"),
+    check('phone').notEmpty().withMessage("phone number is required"),
+    check('age').isInt({ min: 18 }).withMessage("age must be over 18"),
+    check('base64Image').notEmpty().withMessage("base64 image is missing")
+], register);
 export default router;
 //# sourceMappingURL=user.routes.js.map
