12e4828134666ecad4f9f3067d67a8bd84b213ee
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-14 14:12:18 +0300

    [Backend] Implemented revokeToken Function in AuthMiddleware

--- backend/dist/middlewares/auth.middleware.js
+++ backend/dist/middlewares/auth.middleware.js
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import jsonwebtoken from 'jsonwebtoken';
 const jwt = jsonwebtoken;
+export const blackList = [];
 export const isAuthorized = async (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
     //verify the token
-    if (!token) {
+    if (!token || blackList.includes(token)) {
         return res.status(403).json({ message: "Unauthorized" });
     }
     try {
@@ -17,5 +18,13 @@ export const isAuthorized = async (req, res, next) => {
     catch (error) {
         console.log(error);
     }
+};
+export const revokeToken = (token) => {
+    const prevLength = blackList.length;
+    const currentLength = blackList.push(token);
+    if (currentLength > prevLength) {
+        return true;
+    }
+    return false;
 };
 //# sourceMappingURL=auth.middleware.js.map
--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -7,11 +7,13 @@ export interface AuthRequest extends Request {
     user?: JwtPayload;
 }
 
+export const blackList: string[] = []; 
+
 export const isAuthorized = async (req: AuthRequest, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
     
     //verify the token
-    if(!token){
+    if(!token || blackList.includes(token)){
         return res.status(403).json({message: "Unauthorized"});
     }
 
@@ -26,4 +28,13 @@ export const isAuthorized = async (req: AuthRequest, res: Response, next: NextFu
     } catch (error) {
         console.log(error);
     }
+}
+
+export const revokeToken = (token: string) => {
+    const prevLength = blackList.length;
+    const currentLength = blackList.push(token);
+    if(currentLength > prevLength){
+        return true;
+    }
+    return false;
 }
