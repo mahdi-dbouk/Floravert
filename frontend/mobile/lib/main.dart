4740a019557ca0b9a2084c6479afb14f55a0660f
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-16 22:48:31 +0300

    [Frontend > Mobile] Implemented Main Widget

--- frontend/mobile/lib/main.dart
+++ frontend/mobile/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'utils/colors.dart';
+import 'landing.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const App());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        title: "Floravert",
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: primary,
+              backgroundColor: background,
+            )),
+        home: const Landing());
   }
 }
