b331425f58ef48364d6a7b7e4caa7e0aab72fe00
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-18 16:03:53 +0300

    [Backend] Created Updated Contact Schema

--- backend/src/models/schemas/contact.schema.ts
+++ backend/src/models/schemas/contact.schema.ts
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const contactSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     lastSeen: {type: Date, required: true},
+    latestMessage: {type: String, required: true},
     blocked: {type: Boolean, required: true}
 }, {timestamps: true});
 
