2ceab62b5f11d93e54a21567f72d47ae8754e939
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-18 16:01:16 +0300

    [Backend] Created Product Schema

---
+++ backend/src/models/schemas/product.schema.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const {model, Schema} = mongoose;
+
+const productSchema = new Schema({
+    name: {type: String, required: true},
+    traderId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    category: {type: String, required: true},
+    ratings: [{type: Number, required: false}],
+    images: [{type: String, required: false}],
+    description: {type: String, required: true},
+    totalQuantity: {type: String, required: true}
+});
+
+export default productSchema;
