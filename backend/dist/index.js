b1c2da11b7e275855bf0ec1c3960887369b60955
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-19 12:03:45 +0300

    [Backend] Set Limit to 50mb in express.json()

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,7 +8,7 @@ import postRoutes from './routes/post.routes.js';
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors(options));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const app : Application = express();
 
 app.use(cors(options));
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
