ef28c034212d0b6b7f156781e21b3b2c3af3f6cc
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-19 14:29:15 +0300

    [Backend] Sending content-type to uploadToS3 in decodeThenSendToS3 in Image Handler module

--- backend/dist/utils/image.handler.js
+++ backend/dist/utils/image.handler.js
@@ -1,11 +1,12 @@
 import { uploadToS3 } from "../config/s3.js";
 export const decodeThenSendToS3 = async (base64Image) => {
     let [format, base64ImageData] = base64Image.split(';base64,');
+    const contentType = format.split(':')[1];
     format = format.split('/').pop();
     console.log(format);
     const filename = `image_${Date.now()}.${format}`;
     const imageBuffer = Buffer.from(base64ImageData, 'base64');
-    const result = await uploadToS3(imageBuffer, filename);
+    const result = await uploadToS3(imageBuffer, filename, contentType);
     console.log(result);
     return result;
 };
--- backend/src/utils/image.handler.ts
+++ backend/src/utils/image.handler.ts
@@ -3,11 +3,12 @@ import {Response} from 'express';
 
 export const decodeThenSendToS3 = async (base64Image : string) => {
     let [format, base64ImageData] = base64Image.split(';base64,');
+    const contentType = format.split(':')[1];
     format = format.split('/').pop();
     console.log(format);
     const filename = `image_${Date.now()}.${format}`;
     const imageBuffer: Buffer = Buffer.from(base64ImageData, 'base64');
-    const result = await uploadToS3(imageBuffer, filename);
+    const result = await uploadToS3(imageBuffer, filename, contentType);
     console.log(result);
     return result;
 }
