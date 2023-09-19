a0afffe49d7c20cd1725f8a30c38c886179cdc0f
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-19 11:59:46 +0300

    [Backend] Implemented processingImages Function

--- backend/dist/utils/image.handler.js
+++ backend/dist/utils/image.handler.js
@@ -9,4 +9,21 @@ export const decodeThenSendToS3 = async (base64Image) => {
     console.log(result);
     return result;
 };
+export const processImages = async (base64Images, res) => {
+    try {
+        const promises = base64Images.map(async (base64Image) => {
+            const awsResponse = await decodeThenSendToS3(base64Image);
+            return awsResponse.Location;
+        });
+        const imageUrls = await Promise.all(promises);
+        console.log(imageUrls.length, "number of images");
+        return imageUrls;
+    }
+    catch (error) {
+        console.error('Error processing images:', error);
+        return res.status(500).json({
+            error: 'An error occurred while processing images'
+        });
+    }
+};
 //# sourceMappingURL=image.handler.js.map
--- backend/src/utils/image.handler.ts
+++ backend/src/utils/image.handler.ts
@@ -1,4 +1,5 @@
 import { uploadToS3 } from "../config/s3.js";
+import {Response} from 'express';
 
 export const decodeThenSendToS3 = async (base64Image : string) => {
     let [format, base64ImageData] = base64Image.split(';base64,');
@@ -10,3 +11,23 @@ export const decodeThenSendToS3 = async (base64Image : string) => {
     console.log(result);
     return result;
 }
+
+export const processImages = async (base64Images: string[], res: Response) => {
+    try {
+      const promises = base64Images.map(async (base64Image: string) => {
+        const awsResponse = await decodeThenSendToS3(base64Image);
+        return awsResponse.Location;
+      });
+  
+      const imageUrls: string[] = await Promise.all(promises);
+      console.log(imageUrls.length, "number of images");
+  
+      return imageUrls;
+  
+    } catch (error) {
+      console.error('Error processing images:', error);
+      return res.status(500).json({
+        error: 'An error occurred while processing images'
+      });
+    }
+  };
