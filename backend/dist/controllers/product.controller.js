118a8ef53c381c2ea70d1b338c3a1350ed0d6abd
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-19 12:02:26 +0300

    [Backend] Implemented createProduct in Product Controller

--- backend/dist/controllers/product.controller.js
+++ backend/dist/controllers/product.controller.js
@@ -1,2 +1,32 @@
-export {};
+import User from "../models/user.model.js";
+import { processImages } from "../utils/image.handler.js";
+export const createProduct = async (req, res) => {
+    const _id = req.user._id;
+    const { name, category, images, description, totalQuantity } = req.body;
+    const imageUrls = await processImages(images, res);
+    try {
+        const updatedUser = await User.findByIdAndUpdate(_id, {
+            $push: {
+                products: {
+                    name,
+                    category,
+                    description,
+                    images: imageUrls,
+                    totalQuantity
+                }
+            }
+        }, { new: true });
+        if (updatedUser) {
+            return res.status(200).json({
+                message: "added successfully",
+                user: updatedUser,
+            });
+        }
+    }
+    catch (error) {
+        return res.status(500).json({
+            error: error
+        });
+    }
+};
 //# sourceMappingURL=product.controller.js.map
---
+++ backend/src/controllers/product.controller.ts
@@ -0,0 +1,40 @@
+import { AuthRequest } from "../middlewares/auth.middleware.js";
+import { Response } from "express";
+import User from "../models/user.model.js";
+import { processImages } from "../utils/image.handler.js";
+
+export const createProduct = async (req: AuthRequest, res: Response) => {
+    const _id = req.user._id;
+    const {name, category, images, description, totalQuantity} = req.body;
+
+
+    const imageUrls = await processImages(images, res);
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(_id, {
+            $push: {
+                products: {
+                    name,
+                    category,
+                    description,
+                    images: imageUrls,
+                    totalQuantity
+                }
+            }
+        },
+        {new: true}
+        );
+
+        if(updatedUser){
+            return res.status(200).json({
+                message: "added successfully",
+                user: updatedUser,
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: error
+        });
+    }
+}
+
