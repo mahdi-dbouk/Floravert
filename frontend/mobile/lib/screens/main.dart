717f8084484edec32a40adf63a9e62449df7e95f
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-21 17:19:44 +0300

    [Frontend > Mobile] Updated Main

---
+++ frontend/mobile/lib/screens/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/screens/login/view/login.dart';
+import 'package:mobile/screens/register.dart';
+import 'package:mobile/screens/setup.dart';
+import '../utils/colors.dart';
+import 'landing.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Floravert",
+      //debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: primary,
+            backgroundColor: background,
+          )),
+      routes: {
+        '/': (context) => const Landing(),
+        '/login': (context) => const Login(),
+        '/register': (context) => Register(),
+        '/setup': (context) => const Setup()
+      },
+    );
+  }
+}
