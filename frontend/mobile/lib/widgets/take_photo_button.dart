b00d2d3e5e1f3031806b4e2fe444c6644249e301
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-24 12:32:06 +0300

    [Frontend > Mobile] Implemented Camera Button

---
+++ frontend/mobile/lib/widgets/take_photo_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+import 'package:mobile/screens/scanresults.dart';
+
+class NavBarImageButton extends StatefulWidget {
+  const NavBarImageButton({super.key});
+
+  @override
+  State<NavBarImageButton> createState() => _NavBarImageButtonState();
+}
+
+class _NavBarImageButtonState extends State<NavBarImageButton> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.camera);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ScannedResult(image: _image)));
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      width: 80,
+      child: FloatingActionButton(
+        onPressed: () {
+          _pickImage();
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.camera_alt_outlined,
+          size: 48,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
