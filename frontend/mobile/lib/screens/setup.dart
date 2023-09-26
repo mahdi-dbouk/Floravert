53713fb4b1d5d55155412eb7e3d168071f6f443e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-26 15:38:59 +0300

    [Frontend > Mobile] Changed Setup To a Consumer Widget

--- frontend/mobile/lib/screens/setup.dart
+++ frontend/mobile/lib/screens/setup.dart
@@ -2,6 +2,7 @@ import 'package:datetime_picker_formfield_new/datetime_picker_formfield.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:mobile/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 import '/widgets/textinput.dart';
 import 'dart:io';
@@ -37,7 +38,6 @@ class _SetupState extends State<Setup> {
     final Map<String, dynamic>? args = arguments as Map<String, dynamic>?;
 
     if (args == null) {
-      // Handle null arguments
       return Scaffold(
         appBar: AppBar(
           title: const Text('Error'),
@@ -50,199 +50,203 @@ class _SetupState extends State<Setup> {
     final String? fullName = arguments!['fullName'];
     final String? email = arguments['email'];
     final String? password = arguments['password'];
-    return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: SingleChildScrollView(
-          child: SafeArea(
-              child: Column(children: [
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/login');
-                    },
-                    child: const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.arrow_back,
-                          color: Colors.black,
-                          size: 32.0,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text("back",
-                            style: TextStyle(color: Colors.black, fontSize: 16))
-                      ],
-                    ))
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Consumer<AuthProvider>(
+      builder: (context, authUserModel, child) => Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SingleChildScrollView(
+            child: SafeArea(
+                child: Column(children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(
-                    "Let's Get You Ready!",
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/login');
+                      },
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.arrow_back,
+                            color: Colors.black,
+                            size: 32.0,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("back",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16))
+                        ],
+                      ))
                 ],
               ),
-            ),
-            Stack(
-              children: [
-                Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.green, width: 2)),
-                  child: CircleAvatar(
-                    radius: 80,
-                    backgroundImage: _image != null
-                        ? FileImage(_image!)
-                        : const AssetImage(
-                                'assets/green_placeholder_avatar.jpg')
-                            as ImageProvider,
-                  ),
-                ),
-                Positioned(
-                    top: 148,
-                    left: 56,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.green, width: 2),
-                          color: Colors.white),
-                      child: IconButton(
-                          color: Colors.green,
-                          onPressed: _pickImage,
-                          icon: const Icon(Icons.add_a_photo)),
-                    ))
-              ],
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            TextInput(
-                controller: userNameFieldInputController,
-                label: "Username",
-                placeholder: "Username",
-                isHidden: false),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: DateTimeField(
-                controller: dateTimeFieldInputController,
-                format: DateFormat('d/M/y'),
-                initialValue: DateTime.now(),
-                readOnly: true,
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 10),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: "Date of Birth*",
-                    hintText: "Date of Birth*",
-                    hintStyle: TextStyle(
-                      color: Colors.grey.shade400,
-                      fontWeight: FontWeight.normal,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.grey.shade400, width: 1),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 1),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    suffixIcon: const Icon(
-                      Icons.calendar_month_outlined,
-                      color: Colors.green,
-                    )),
-                onShowPicker: (context, currentValue) {
-                  return showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(2005),
-                      lastDate: DateTime.now());
-                },
+              const SizedBox(
+                height: 5,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: InternationalPhoneNumberInput(
-                onInputChanged: (PhoneNumber phone) {
-                  phoneNumber = phone.phoneNumber!;
-                },
-                selectorConfig: const SelectorConfig(
-                  selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
-                  setSelectorButtonAsPrefixIcon: true,
-                  leadingPadding: 10.0,
-                ),
-                inputDecoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 10),
-                    fillColor: Colors.white,
-                    filled: true,
-                    labelText: "Phone*",
-                    hintText: "Phone*",
-                    hintStyle: TextStyle(
-                      color: Colors.grey.shade400,
-                      fontWeight: FontWeight.normal,
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Let's Get You Ready!",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.grey.shade400, width: 1),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 1),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    suffixIcon: const Icon(
-                      Icons.phone_android_outlined,
-                      color: Colors.green,
-                    )),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(
-                        Theme.of(context).colorScheme.primary),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text("Create Account",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold)),
-                  ),
+                  ],
                 ),
               ),
-            ),
-          ])),
-        ));
+              Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.green, width: 2)),
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundImage: _image != null
+                          ? FileImage(_image!)
+                          : const AssetImage(
+                                  'assets/green_placeholder_avatar.jpg')
+                              as ImageProvider,
+                    ),
+                  ),
+                  Positioned(
+                      top: 148,
+                      left: 56,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.green, width: 2),
+                            color: Colors.white),
+                        child: IconButton(
+                            color: Colors.green,
+                            onPressed: _pickImage,
+                            icon: const Icon(Icons.add_a_photo)),
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              TextInput(
+                  controller: userNameFieldInputController,
+                  label: "Username",
+                  placeholder: "Username",
+                  isHidden: false),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: DateTimeField(
+                  controller: dateTimeFieldInputController,
+                  format: DateFormat('d/M/y'),
+                  initialValue: DateTime.now(),
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: "Date of Birth*",
+                      hintText: "Date of Birth*",
+                      hintStyle: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.grey.shade400, width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      suffixIcon: const Icon(
+                        Icons.calendar_month_outlined,
+                        color: Colors.green,
+                      )),
+                  onShowPicker: (context, currentValue) {
+                    return showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2005),
+                        lastDate: DateTime.now());
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: InternationalPhoneNumberInput(
+                  onInputChanged: (PhoneNumber phone) {
+                    phoneNumber = phone.phoneNumber!;
+                  },
+                  selectorConfig: const SelectorConfig(
+                    selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                    setSelectorButtonAsPrefixIcon: true,
+                    leadingPadding: 10.0,
+                  ),
+                  inputDecoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: "Phone*",
+                      hintText: "Phone*",
+                      hintStyle: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.grey.shade400, width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      suffixIcon: const Icon(
+                        Icons.phone_android_outlined,
+                        color: Colors.green,
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Theme.of(context).colorScheme.primary),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text("Create Account",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ),
+              ),
+            ])),
+          )),
+    );
   }
 }
