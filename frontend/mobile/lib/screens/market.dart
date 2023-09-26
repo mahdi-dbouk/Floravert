64a1bd79b4ee40d86a580b6fde8b186e4e1bcfed
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-26 14:07:33 +0300

    [Frontend > Mobile] Implemented Market Page (part 2)

--- frontend/mobile/lib/screens/market.dart
+++ frontend/mobile/lib/screens/market.dart
@@ -22,6 +22,68 @@ class _MarketState extends State<Market> {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 60,
+                  child: TextField(
+                    controller: _searchInputController,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      labelText: "Search",
+                      hintStyle: const TextStyle(color: Colors.black38),
+                      prefixIcon: Icon(Icons.search,
+                          color: Theme.of(context).colorScheme.primary),
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                      focusColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.grey.shade400, width: 1),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: IconButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          backgroundColor: MaterialStatePropertyAll(
+                              Theme.of(context).colorScheme.primary)),
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      )),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
