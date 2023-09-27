837b4fbd95565be8c245ea072d5512ff1cd1dcf9
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 16:45:46 +0300

    [Frontend > Mobile] Updated Product Data Model to Accomodate For Product Schema Change

--- frontend/mobile/lib/models/product_data_model.dart
+++ frontend/mobile/lib/models/product_data_model.dart
@@ -2,6 +2,7 @@ class Product {
   String? name;
   String? category;
   List<int>? ratings;
+  double? price;
   List<String>? images;
   String? description;
   int? totalQuantity;
@@ -10,6 +11,7 @@ class Product {
       {this.name,
       this.category,
       this.ratings,
+      this.price,
       this.images,
       this.description,
       this.totalQuantity});
@@ -17,6 +19,7 @@ class Product {
   Product.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     category = json['category'];
+    price = json['price'];
     ratings = json['ratings'].cast<int>();
     images = json['images'].cast<String>();
     description = json['description'];
@@ -27,6 +30,7 @@ class Product {
     final Map<String, dynamic> data = {};
     data['name'] = name;
     data['category'] = category;
+    data['price'] = price;
     data['ratings'] = ratings;
     data['images'] = images;
     data['description'] = description;
