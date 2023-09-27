8e4581de8cb9b4f4dcf50b283524656249fc8c4b
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 10:12:32 +0300

    [Backend] Updated Cors allowedOrigins to Allow All Only for Development Environment

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,8 +1,7 @@
 import { CorsOptions } from "cors";
 
 const allowedOrigins: string[] = [
-    'http://localhost:8000',
-    'null',
+    '*',
 ];
 const options: CorsOptions = {
     origin: allowedOrigins
