dcd49c81fd11a8de63e2edb8834ab35a3125493c
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 16:35:55 +0300

    [Frontend > Mobile] Added price Field to Product Schema

--- backend/src/models/schemas/product.schema.ts
+++ backend/src/models/schemas/product.schema.ts
@@ -5,6 +5,7 @@ const productSchema = new Schema({
     name: {type: String, required: true},
     category: {type: String, required: true},
     ratings: [{type: Number, required: false}],
+    price: {type: Number, required: false},
     images: [{type: String, required: false}],
     description: {type: String, required: true},
     totalQuantity: {type: String, required: true}
