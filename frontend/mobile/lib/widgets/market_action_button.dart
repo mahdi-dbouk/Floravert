720d64a167ff5c445888abc54261d6fcec744637
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 14:51:58 +0300

    [Frontend > Mobile] Created Market Action Button

---
+++ frontend/mobile/lib/widgets/market_action_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarketActionButton extends StatefulWidget {
+  const MarketActionButton({super.key});
+
+  @override
+  State<MarketActionButton> createState() => _MarketActionButtonState();
+}
+
+class _MarketActionButtonState extends State<MarketActionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
