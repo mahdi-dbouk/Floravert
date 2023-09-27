5fb9a95c30633f59cd2e59929cdd5ef26506c2c3
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 10:19:28 +0300

    [Backend] Implementing Chat Controller (Part 1)

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -2,6 +2,21 @@ import {Response} from 'express';
 import { AuthRequest } from '../middlewares/auth.middleware.js';
 import User from '../models/user.model.js';
 
+export const chatsHandler = (io) => {
+    io.on('connection', (socket)=>{
+        console.log(`user: ${socket.id}`);
+        socket.on("test", (args) => {
+            console.log(args);
+        });
+    
+    
+        socket.on('disconnect', () => {
+            console.log(`${socket.id} disconnected`)
+        })
+    });
+}
+
+
 
 export const createContact = async (req: AuthRequest, res: Response) => {
     const _id = req.user._id;
@@ -28,3 +43,21 @@ export const createContact = async (req: AuthRequest, res: Response) => {
         });
     }
 }
+
+export const createMessage = async (req: AuthRequest, res: Response) => {
+    const _id = req.user._id;
+
+}
+
+export const getAllMessages = async (req: AuthRequest, res:Response) => {
+
+}
+
+export const deleteChat = async (req: AuthRequest, res: Response) => {
+    
+}
+export const deleteContact = async (req: AuthRequest, res: Response) => {
+    
+}
+
+
