ef92d3cb56d4166f82c19adb91447e0f0d22e7ea
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 16:34:49 +0300

    [Frontend > Mobile] Set Home Screen to Be a Consumer of Product Provider

--- frontend/mobile/lib/screens/home.dart
+++ frontend/mobile/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/providers/auth_provider.dart';
+import 'package:mobile/providers/product_provider.dart';
 import 'package:mobile/providers/scanned_plant_provider.dart';
 import 'package:mobile/screens/map_page.dart';
 import 'package:mobile/screens/market.dart';
@@ -29,109 +30,115 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer2<AuthProvider, ScannedPlantProvider>(
-      builder: (context, authUserModel, scannedPlantModel, child) => Scaffold(
-          drawer: Drawer(
-              backgroundColor: Theme.of(context).primaryColor,
-              child: Column(
-                children: [
-                  DrawerHeader(
-                      child: Column(
+    return Consumer3<AuthProvider, ScannedPlantProvider, ProductProvider>(
+      builder: (context, authUserModel, scannedPlantModel, productModel,
+              child) =>
+          Scaffold(
+              drawer: Drawer(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Column(
                     children: [
-                      Center(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(200),
-                              border:
-                                  Border.all(color: Colors.yellow, width: 2)),
-                          child: CircleAvatar(
-                            radius: 40,
-                            backgroundImage: NetworkImage(
-                                authUserModel.user.pictureUrl ?? ""),
+                      DrawerHeader(
+                          child: Column(
+                        children: [
+                          Center(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(200),
+                                  border: Border.all(
+                                      color: Colors.yellow, width: 2)),
+                              child: CircleAvatar(
+                                radius: 40,
+                                backgroundImage: NetworkImage(
+                                    authUserModel.user.pictureUrl ?? ""),
+                              ),
+                            ),
                           ),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                authUserModel.user.username ?? "username",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 22),
+                              )),
+                        ],
+                      )),
+                      const ListTile(
+                        leading: Icon(
+                          Icons.stars_rounded,
+                          color: Colors.amber,
+                          size: 30,
+                        ),
+                        title: Text(
+                          "Become a Trader",
+                          style: TextStyle(fontSize: 22, color: Colors.white),
                         ),
                       ),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            authUserModel.user.username ?? "username",
-                            style: TextStyle(color: Colors.white, fontSize: 22),
-                          )),
+                      const Expanded(child: SizedBox()),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: SizedBox(
+                            width: 200,
+                            height: 60,
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Logout",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ))),
+                      )
                     ],
                   )),
-                  const ListTile(
-                    leading: Icon(
-                      Icons.stars_rounded,
-                      color: Colors.amber,
-                      size: 30,
-                    ),
-                    title: Text(
-                      "Become a Trader",
-                      style: TextStyle(fontSize: 22, color: Colors.white),
-                    ),
-                  ),
-                  const Expanded(child: SizedBox()),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: SizedBox(
-                        width: 200,
-                        height: 60,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: const Text(
-                              "Logout",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ))),
-                  )
+              appBar: const CustomAppBar(),
+              body: switch (page) {
+                'home' => HomePage(
+                    authUserModel: authUserModel,
+                    scannedPlantModel: scannedPlantModel),
+                'market' => const Market(),
+                'map' => const MapPage(),
+                String() => null,
+              },
+              floatingActionButton: switch (page) {
+                'home' => const NavBarImageButton(),
+                'market' => const MarketActionButton(),
+                'map' => const MapActionButton(),
+                String() => null,
+              },
+              bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _currentIndex,
+                backgroundColor: Colors.white,
+                items: [
+                  BottomNavigationBarItem(
+                      label: 'Home',
+                      icon: Icon(Icons.home,
+                          size: 28, color: Colors.grey.shade400),
+                      activeIcon: Icon(Icons.home,
+                          size: 28, color: Theme.of(context).primaryColor)),
+                  BottomNavigationBarItem(
+                      label: 'Map',
+                      icon: Icon(Icons.map,
+                          size: 28, color: Colors.grey.shade400),
+                      activeIcon: Icon(Icons.map,
+                          size: 28, color: Theme.of(context).primaryColor)),
+                  BottomNavigationBarItem(
+                      label: 'Market',
+                      icon: Icon(Icons.store,
+                          size: 28, color: Colors.grey.shade400),
+                      activeIcon: Icon(Icons.store,
+                          size: 28, color: Theme.of(context).primaryColor)),
                 ],
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                    page = _loadWidget(_currentIndex);
+                  });
+                },
               )),
-          appBar: const CustomAppBar(),
-          body: switch (page) {
-            'home' => HomePage(
-                authUserModel: authUserModel,
-                scannedPlantModel: scannedPlantModel),
-            'market' => const Market(),
-            'map' => const MapPage(),
-            String() => null,
-          },
-          floatingActionButton: switch (page) {
-            'home' => const NavBarImageButton(),
-            'market' => const MarketActionButton(),
-            'map' => const MapActionButton(),
-            String() => null,
-          },
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _currentIndex,
-            backgroundColor: Colors.white,
-            items: [
-              BottomNavigationBarItem(
-                  label: 'Home',
-                  icon: Icon(Icons.home, size: 28, color: Colors.grey.shade400),
-                  activeIcon: Icon(Icons.home,
-                      size: 28, color: Theme.of(context).primaryColor)),
-              BottomNavigationBarItem(
-                  label: 'Map',
-                  icon: Icon(Icons.map, size: 28, color: Colors.grey.shade400),
-                  activeIcon: Icon(Icons.map,
-                      size: 28, color: Theme.of(context).primaryColor)),
-              BottomNavigationBarItem(
-                  label: 'Market',
-                  icon:
-                      Icon(Icons.store, size: 28, color: Colors.grey.shade400),
-                  activeIcon: Icon(Icons.store,
-                      size: 28, color: Theme.of(context).primaryColor)),
-            ],
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-                page = _loadWidget(_currentIndex);
-              });
-            },
-          )),
     );
   }
 }
