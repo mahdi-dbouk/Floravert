d07714ad34ab294927f5a57c04ffbb0033105d46
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 14:51:44 +0300

    [Frontend > Mobile] Created Map Action Button

---
+++ frontend/mobile/lib/widgets/map_action_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapActionButton extends StatefulWidget {
+  const MapActionButton({super.key});
+
+  @override
+  State<MapActionButton> createState() => _MapActionButtonState();
+}
+
+class _MapActionButtonState extends State<MapActionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
