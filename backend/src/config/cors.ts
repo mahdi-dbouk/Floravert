0e6275cffb1bf71322c8feac47f16725a40b8050
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 12:44:49 +0300

    [Frontend > Mobile] Updated cors.ts (local, to be removed)

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,6 +1,9 @@
 import { CorsOptions } from "cors";
 
 const allowedOrigins: string[] = [
+    'http://localhost:8000',
+    'http://192.168.0.103:8000',
+    'http://localhost:3000',
     '*',
 ];
 const options: CorsOptions = {
