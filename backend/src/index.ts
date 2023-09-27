5a04842eb7f9c32e4149392735895647a00f4d01
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 10:10:55 +0300

    [Backend] Implemented Socket.IO Logic in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,34 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 import 'dotenv/config';
-import mongoDBConn from "./config/connection.js";
+import mongoDBConn from './config/connection.js';
 import cors from 'cors';
 import options from "./config/cors.js";
 import userRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
+import * as SocketIO from 'socket.io';
+import * as http from 'http';
+import { chatsHandler } from './controllers/chat.controller.js';
+
 
 const PORT : String|Number = process.env.PORT || 8000;
 
 const app : Application = express();
 
 app.use(cors(options));
+const server: http.Server = http.createServer(app);
+
+const io = new SocketIO.Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['*']
+    }
+});
+
+chatsHandler(io);
 
 app.use(express.json({limit: '50mb'}));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
