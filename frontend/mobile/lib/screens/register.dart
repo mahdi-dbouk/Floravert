191bd9129e71f1ccc299e981dc081a48dd2ccff3
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 12:38:08 +0300

    [Frontend > Mobile] Updated UI for Register Screen and Added the Logo

--- frontend/mobile/lib/screens/register.dart
+++ frontend/mobile/lib/screens/register.dart
@@ -1,212 +1,155 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:mobile/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 import '/widgets/textinput.dart';
 
 class Register extends StatefulWidget {
-  final fullNameInputController = TextEditingController();
-
-  Register({super.key});
+  const Register({super.key});
 
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _fullNameInputController =
+      TextEditingController();
+  final TextEditingController _emailInputController = TextEditingController();
+  final TextEditingController _passwordInputController =
+      TextEditingController();
+  final TextEditingController _confirmPasswordInputController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: SingleChildScrollView(
-          child: SafeArea(
-              child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/login');
-                      },
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.arrow_back,
-                            color: Colors.black,
-                            size: 32.0,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text("back",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 16))
-                        ],
-                      ))
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10),
-                child: Center(
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
+    return Consumer<AuthProvider>(
+      builder: (context, authUserModel, child) => Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          body: SingleChildScrollView(
+            child: SafeArea(
+                child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              TextInput(
-                  controller: widget.fullNameInputController,
-                  label: "Full Name",
-                  placeholder: "First Name",
-                  isHidden: false),
-              TextInput(
-                  controller: TextEditingController(),
-                  label: "Email",
-                  placeholder: "Email",
-                  isHidden: false),
-              TextInput(
-                  controller: TextEditingController(),
-                  label: "Password",
-                  placeholder: "Password",
-                  isHidden: true),
-              TextInput(
-                  controller: TextEditingController(),
-                  label: "Confirm Password",
-                  placeholder: "Confirm Password",
-                  isHidden: true),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/setup');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(
-                          Theme.of(context).colorScheme.primary),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text("Sign Up",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold)),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey.shade400,
-                    ),
-                  ),
-                  Text(
-                    "OR",
-                    style: TextStyle(color: Colors.grey.shade400),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey.shade400,
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print(context);
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          const MaterialStatePropertyAll(Colors.white),
-                      side: MaterialStatePropertyAll(BorderSide(
-                        color: Colors.grey.shade200,
-                      )),
-                      padding: const MaterialStatePropertyAll(
-                          EdgeInsets.symmetric(horizontal: 8)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const SizedBox(
-                                  width: 8,
-                                ),
-                                const Text(
-                                  "Sign Up with Google",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const Spacer(
-                                  flex: 1,
-                                ),
-                                Image.asset(
-                                  'assets/google.svg.png',
-                                  scale: 25,
-                                )
-                              ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/login');
+                        },
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.arrow_back,
+                              color: Colors.black,
+                              size: 32.0,
                             ),
-                          ),
-                        ],
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text("back",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16))
+                          ],
+                        ))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/floravert_logo.png',
+                      scale: 5,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text.rich(TextSpan(children: [
-                TextSpan(
-                    text: "Already a user?",
-                    style:
-                        TextStyle(color: Colors.grey.shade500, fontSize: 16)),
-                const TextSpan(text: " "),
-                TextSpan(
-                    text: "Login",
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 16),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Login()));
-                      })
-              ]))
-            ],
+                TextInput(
+                    controller: _fullNameInputController,
+                    label: "Full Name",
+                    placeholder: "First Name",
+                    isHidden: false),
+                TextInput(
+                    controller: _emailInputController,
+                    label: "Email",
+                    placeholder: "Email",
+                    isHidden: false),
+                TextInput(
+                    controller: _passwordInputController,
+                    label: "Password",
+                    placeholder: "Password",
+                    isHidden: true),
+                TextInput(
+                    controller: _confirmPasswordInputController,
+                    label: "Confirm Password",
+                    placeholder: "Confirm Password",
+                    isHidden: true),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/setup', arguments: {
+                          "email": _emailInputController.text,
+                          "fullname": _fullNameInputController.text,
+                          "password": _passwordInputController.text
+                        });
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(
+                            Theme.of(context).colorScheme.primary),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text("Sign Up",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text.rich(TextSpan(children: [
+                  TextSpan(
+                      text: "Already a user?",
+                      style:
+                          TextStyle(color: Colors.grey.shade500, fontSize: 16)),
+                  const TextSpan(text: " "),
+                  TextSpan(
+                      text: "Login",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => Navigator.of(context)
+                            .pushReplacementNamed('/login'))
+                ]))
+              ],
+            )),
           )),
-        ));
+    );
   }
 }
