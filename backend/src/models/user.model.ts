b42b00611f95385ae112cf198a63dd40eb92bc37
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-27 10:16:25 +0300

    [Backend] Updated User Model to Include Username Field

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -10,6 +10,7 @@ const { model, Schema } = mongoose;
 const userSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
+    username: {type: String, required: true},
     email: { type: String, required: true },
     hashedPassword: { type: String, required: true },
     age: {type: Number, required: true },
