e013910a7d0f0d0fd4d8cd5509f5971ef26a2b70
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-28 19:13:12 +0300

    [Frontend > Mobile] Set Add Product Page to be a Consumer of Product Provider

--- frontend/mobile/lib/screens/add_product.dart
+++ frontend/mobile/lib/screens/add_product.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/providers/product_provider.dart';
 import 'package:mobile/widgets/textinput.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class AddProductForm extends StatefulWidget {
   const AddProductForm({super.key});
@@ -43,170 +45,172 @@ class _AddProductFormState extends State<AddProductForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-      child: SafeArea(
-          child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.black,
-                        size: 32.0,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text("back",
-                          style: TextStyle(color: Colors.black, fontSize: 16))
-                    ],
-                  ))
-            ],
-          ),
-          (selectedImages.length != 0)
-              ? CarouselSlider.builder(
-                  itemCount: selectedImages.length,
-                  itemBuilder: (context, index, realIndex) {
-                    final File image = selectedImages[index];
-                    return Container(
-                      decoration: BoxDecoration(boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 2))
-                      ]),
-                      margin: const EdgeInsets.symmetric(horizontal: 5),
-                      width: MediaQuery.of(context).size.width,
-                      height: 300,
-                      padding: EdgeInsets.zero,
-                      child: Image.file(
-                        image,
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                  options: CarouselOptions(
-                      viewportFraction: 0.8,
-                      enlargeCenterPage: true,
-                      autoPlay: true,
-                      autoPlayInterval: const Duration(seconds: 3),
-                      height: 300,
-                      aspectRatio: 1 / 1,
-                      enableInfiniteScroll: false))
-              : Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  padding: EdgeInsets.zero,
-                  child: Image.asset(
-                    './assets/placeholder-square.png',
-                    fit: BoxFit.cover,
-                  )),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton.icon(
-            onPressed: () {
-              getImages();
-            },
-            icon: const Icon(Icons.add_a_photo_outlined),
-            label: const Text("Append Image"),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextInput(
-              maxLines: 1,
-              controller: _titleInputController,
-              label: "Title",
-              placeholder: "Title",
-              isHidden: false),
-          TextInput(
-              maxLines: 6,
-              controller: _descriptionInputController,
-              label: "Desciption",
-              placeholder: "Description",
-              isHidden: false),
-          TextInput(
-              maxLines: 1,
-              controller: _quantityInputController,
-              label: "Quantity",
-              placeholder: "Quantity",
-              isHidden: false),
-          TextInput(
-              maxLines: 1,
-              controller: _priceInputController,
-              label: "Price",
-              placeholder: "Price",
-              isHidden: false),
-          const Center(
-            child: Text(
-              "Category",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
+    return Consumer<ProductProvider>(
+      builder: (BuildContext context, productModel, child) => Scaffold(
+          body: SingleChildScrollView(
+        child: SafeArea(
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            child: Row(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Expanded(
-                  child: RadioListTile(
-                      value: "natural",
-                      title: const Text("Natural"),
-                      groupValue: selectedOption,
-                      onChanged: (val) {
-                        setState(() {
-                          selectedOption = val!;
-                        });
-                      }),
-                ),
-                Expanded(
-                  child: RadioListTile(
-                      value: "processed",
-                      title: const Text("Processed"),
-                      groupValue: selectedOption,
-                      onChanged: (val) {
-                        setState(() {
-                          selectedOption = val!;
-                        });
-                      }),
-                )
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                          size: 32.0,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("back",
+                            style: TextStyle(color: Colors.black, fontSize: 16))
+                      ],
+                    ))
               ],
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 20),
-            height: 60,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(
-                      Theme.of(context).colorScheme.primary)),
-              child: const Text(
-                "Add Product",
-                style: TextStyle(color: Colors.white),
+            (selectedImages.length != 0)
+                ? CarouselSlider.builder(
+                    itemCount: selectedImages.length,
+                    itemBuilder: (context, index, realIndex) {
+                      final File image = selectedImages[index];
+                      return Container(
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 2))
+                        ]),
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        width: MediaQuery.of(context).size.width,
+                        height: 300,
+                        padding: EdgeInsets.zero,
+                        child: Image.file(
+                          image,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                    options: CarouselOptions(
+                        viewportFraction: 0.8,
+                        enlargeCenterPage: true,
+                        autoPlay: true,
+                        autoPlayInterval: const Duration(seconds: 3),
+                        height: 300,
+                        aspectRatio: 1 / 1,
+                        enableInfiniteScroll: false))
+                : Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 300,
+                    padding: EdgeInsets.zero,
+                    child: Image.asset(
+                      './assets/placeholder-square.png',
+                      fit: BoxFit.cover,
+                    )),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                getImages();
+              },
+              icon: const Icon(Icons.add_a_photo_outlined),
+              label: const Text("Append Image"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextInput(
+                maxLines: 1,
+                controller: _titleInputController,
+                label: "Title",
+                placeholder: "Title",
+                isHidden: false),
+            TextInput(
+                maxLines: 6,
+                controller: _descriptionInputController,
+                label: "Desciption",
+                placeholder: "Description",
+                isHidden: false),
+            TextInput(
+                maxLines: 1,
+                controller: _quantityInputController,
+                label: "Quantity",
+                placeholder: "Quantity",
+                isHidden: false),
+            TextInput(
+                maxLines: 1,
+                controller: _priceInputController,
+                label: "Price",
+                placeholder: "Price",
+                isHidden: false),
+            const Center(
+              child: Text(
+                "Category",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
               ),
             ),
-          )
-        ],
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile(
+                        value: "natural",
+                        title: const Text("Natural"),
+                        groupValue: selectedOption,
+                        onChanged: (val) {
+                          setState(() {
+                            selectedOption = val!;
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: RadioListTile(
+                        value: "processed",
+                        title: const Text("Processed"),
+                        groupValue: selectedOption,
+                        onChanged: (val) {
+                          setState(() {
+                            selectedOption = val!;
+                          });
+                        }),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              height: 60,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Theme.of(context).colorScheme.primary)),
+                child: const Text(
+                  "Add Product",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        )),
       )),
-    ));
+    );
   }
 }
