eb2be3af3ad97d3f03ff6e18f2f2036ca272ccb2
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-28 15:25:38 +0300

    [Frontend > Mobile] Changed Navigation Method to Push Replacement Name in Login Screen

--- frontend/mobile/lib/screens/login.dart
+++ frontend/mobile/lib/screens/login.dart
@@ -108,7 +108,7 @@ class _LoginState extends State<Login> {
                           setState(() {});
                         }
 
-                        Navigator.of(context).pushNamed('/');
+                        Navigator.of(context).pushReplacementNamed('/');
                       },
                       style: ButtonStyle(
                         backgroundColor: MaterialStatePropertyAll(
