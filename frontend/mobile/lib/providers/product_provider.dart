8f556d5a4fccb02fe69e658e7ec34424e68d5a77
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-09-28 22:42:40 +0300

    [Frontend > Mobile] Fixed a Typo in Product Provider

--- frontend/mobile/lib/providers/product_provider.dart
+++ frontend/mobile/lib/providers/product_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mobile/config/send_request.dart';
 import 'package:mobile/models/product_data_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import '../utils/image_handling.dart';
 
 class ProductProvider extends ChangeNotifier {
   List<Product> _products = [];
@@ -9,13 +11,16 @@ class ProductProvider extends ChangeNotifier {
 
   List<Product> get products => _products;
 
-  void setProducts(List<Product> plants) {
+  void setProducts(List<Product> products) {
     _products = products;
   }
 
   void getAllProducts() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
+    if (token == null) {
+      throw Exception("Token is Null!");
+    }
     try {
       dynamic response = await sendRequest('/user/products', 'get', {}, token);
       List<Product> products = [];
@@ -24,8 +29,42 @@ class ProductProvider extends ChangeNotifier {
       });
       _products = products;
     } catch (e) {
-      print(e);
+      rethrow;
     }
+    notifyListeners();
+  }
+
+  void createProduct(String name, String description, String category,
+      String totalQuantity, String price, List<File> images) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    if (token == null) {
+      throw Exception("Token is Null!");
+    }
+    List<String> base64Images = [];
+    String base64Image = '';
+    for (var i = 0; i < images.length; i++) {
+      base64Image = await Base64ImageHandler().convertToBase64(images[i]);
+      base64Images.add(base64Image);
+    }
+
+    Product p = Product(
+        name: name,
+        category: category,
+        description: description,
+        price: double.parse(price),
+        totalQuantity: int.parse(totalQuantity),
+        images: base64Images);
+
+    Map<String, dynamic> data = p.toJson();
+
+    try {
+      dynamic response =
+          await sendRequest('/user/trader/product/add', 'post', data, token);
+    } catch (e) {
+      rethrow;
+    }
+
     notifyListeners();
   }
 }
