605b012e38f9bc0ec2f72d14f27ef35324b3ee2e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 11:59:22 +0300

    [Frontend > Mobile] Added Custom App Bar to AuthProvider Consumers

--- frontend/mobile/lib/widgets/custom_appbar.dart
+++ frontend/mobile/lib/widgets/custom_appbar.dart
@@ -8,71 +8,73 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        "Floravert",
-        style: TextStyle(
-          color: Theme.of(context).primaryColor,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
+    return Consumer<AuthProvider>(
+      builder: (BuildContext context, authUserModel, child) => AppBar(
+        title: Text(
+          "Floravert",
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-      centerTitle: true,
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications_outlined,
-                size: 28,
+        centerTitle: true,
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_outlined,
+                  size: 28,
+                ),
+                color: Theme.of(context).primaryColor,
               ),
-              color: Theme.of(context).primaryColor,
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/contacts');
-              },
-              icon: const Icon(
-                Icons.chat_bubble_rounded,
-                size: 26,
-              ),
-              color: Theme.of(context).primaryColor,
-            ),
-          ],
-        ),
-      ],
-      leading: Builder(builder: (context) {
-        return Row(
-          children: [
-            SizedBox(
-              width: 2,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushNamed('/profile');
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/contacts');
                 },
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(200),
-                      border: Border.all(
-                          width: 2,
-                          color: Theme.of(context).colorScheme.primary)),
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundImage: NetworkImage(
-                        Provider.of<AuthProvider>(context).user.pictureUrl!),
+                icon: const Icon(
+                  Icons.chat_bubble_rounded,
+                  size: 26,
+                ),
+                color: Theme.of(context).primaryColor,
+              ),
+            ],
+          ),
+        ],
+        leading: Builder(builder: (context) {
+          return Row(
+            children: [
+              SizedBox(
+                width: 2,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/profile');
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(200),
+                        border: Border.all(
+                            width: 2,
+                            color: Theme.of(context).colorScheme.primary)),
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage:
+                          NetworkImage(authUserModel.user.pictureUrl!),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        );
-      }),
+            ],
+          );
+        }),
+      ),
     );
   }
 }
