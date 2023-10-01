b3162c9e75d8e462fea7b1172edfe267d3dd398e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 23:56:35 +0300

    [Frontend > Mobile] Removed Unnecessary Imports

--- frontend/mobile/lib/providers/map_provider.dart
+++ frontend/mobile/lib/providers/map_provider.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 
 class MapProvider extends ChangeNotifier {
   double lat = 0.0;
