77603d39508045d94c0516d397e731b50686176f
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 12:33:24 +0300

    Revert "[Frontend > Mobile] Removed User Avatar From Home Welcome Banner"
    This reverts commit 84bee5d0c0cdfe16f26e1dd0fca33b8e94187b99.

--- frontend/mobile/lib/widgets/home_welcome_banner.dart
+++ frontend/mobile/lib/widgets/home_welcome_banner.dart
@@ -18,24 +18,34 @@ class _WelcomeBannerState extends State<WelcomeBanner> {
       child: Padding(
         padding: const EdgeInsets.all(10),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.green, width: 3)),
+              child: CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(widget.url ??
+                    'https://cdn3d.iconscout.com/3d/premium/thumb/hrd-manager-9642497-7825761.png?f=webp'),
+              ),
+            ),
             const SizedBox(
               width: 20,
             ),
             Text.rich(TextSpan(children: [
-              const TextSpan(text: "Hello, ", style: TextStyle(fontSize: 22)),
+              const TextSpan(text: "Hello, ", style: TextStyle(fontSize: 18)),
               TextSpan(
                   text: widget.name,
                   style: TextStyle(
                       color: Theme.of(context).primaryColor,
-                      fontSize: 22,
+                      fontSize: 18,
                       fontWeight: FontWeight.w600)),
               TextSpan(
                   text: "!",
                   style: TextStyle(
                       color: Theme.of(context).primaryColor,
-                      fontSize: 22,
+                      fontSize: 18,
                       fontWeight: FontWeight.w600))
             ])),
           ],
