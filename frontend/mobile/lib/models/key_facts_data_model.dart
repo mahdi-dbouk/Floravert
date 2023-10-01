eef3db81b86b532d555be7d4b475753b3a5eba38
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 17:33:37 +0300

    [Frontend > Mobile] Updated KeyFacts in Scanned Plant Model

--- frontend/mobile/lib/models/key_facts_data_model.dart
+++ frontend/mobile/lib/models/key_facts_data_model.dart
@@ -1,24 +1,30 @@
 class KeyFacts {
   String? edibility;
   String? seasonality;
+  String? toxicity;
   String? habitat;
   String? uses;
+  String? conservationStatus;
 
   KeyFacts({this.edibility, this.seasonality, this.habitat, this.uses});
 
   KeyFacts.fromJson(Map<String, dynamic> json) {
     edibility = json['edibility'];
     seasonality = json['seasonality'];
+    toxicity = json['toxicity'];
     habitat = json['habitat'];
     uses = json['uses'];
+    conservationStatus = json['status'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = {};
     data['edibility'] = edibility;
     data['seasonality'] = seasonality;
+    data['toxicity'] = toxicity;
     data['habitat'] = habitat;
     data['uses'] = uses;
+    data['status'] = conservationStatus;
     return data;
   }
 }
