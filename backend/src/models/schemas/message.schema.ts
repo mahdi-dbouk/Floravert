0b70247a8b1363c9fd10a1647360c64aad23324c
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 09:15:48 +0300

    [Frontend > Mobile] Added Receiver Field to Message Schema

--- backend/src/models/schemas/message.schema.ts
+++ backend/src/models/schemas/message.schema.ts
@@ -3,6 +3,7 @@ const {Schema} = mongoose;
 
 const messageSchema = new Schema({
   sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   content: { type: String, required: true },
 }, {timestamps: true});
 
