29c7bc6a6a66cc7f6b2761fa504563c777672c9d
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-01 17:32:26 +0300

    [Frontend > Mobile] Implemented getLocation() in Map Provider

--- frontend/mobile/lib/providers/map_provider.dart
+++ frontend/mobile/lib/providers/map_provider.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapProvider extends ChangeNotifier {}
+class MapProvider extends ChangeNotifier {
+  double lat = 0.0;
+  double lng = 0.0;
+  void getLocation(double lat_, double lng_) {
+    lat = lat_;
+    lng = lng_;
+
+    notifyListeners();
+  }
+}
